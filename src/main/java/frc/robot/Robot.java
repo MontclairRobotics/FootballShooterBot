61d6c195dd0dfcea4f3118696024c0b0ffcaa673
Josh <joshmstout@icloud.com>
2019-12-09 15:12:37 -0500

    Added Vendor Deps for new motors and changed code for the motors

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -34,12 +34,11 @@ public class Robot extends TimedRobot {
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
    */
-
+  
   @Override
   public void robotInit() {
     leftLaunch = new CANSparkMax(4, MotorType.kBrushless);
     rightLaunch = new CANSparkMax(5, MotorType.kBrushless);
-    
   }
   public void teleopInit() {
     opStick = new Joystick(0);
